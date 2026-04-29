bf57d7eaffcaa6f0086322dd4e86e9f7e75b5409
Akshat-J0in <229117570+Akshat-J0in@users.noreply.github.com>
2026-04-29 11:23:59 +0530

    Search in a sorted 2D matrix

---
+++ Binary_Search/search_in_2d_array.py
@@ -0,0 +1,29 @@
+#Problem Statement:
+"""
+You have been given a 2-D array 'mat' of size 'N x M' where 'N' and 'M' denote the number of rows and columns, respectively.
+The elements of each row are sorted in non-decreasing order.
+Moreover, the first element of a row is greater than the last element of the previous row (if it exists).
+You are given an integer ‘target’, and your task is to find if it exists in the given 'mat' or not.
+"""
+
+def find_ele(arr,k,n,m):
+    low = 0
+    high = (n*m) - 1
+    while low <= high:
+        mid = (low+high)//2
+        row = mid // m
+        col = mid % m
+
+        if arr[row][col] == k:
+            return True
+        elif arr[row][col] < k:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return False
+
+arr = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+target = 78
+n = 3
+m = 4
+print(find_ele(arr,target,n,m))
