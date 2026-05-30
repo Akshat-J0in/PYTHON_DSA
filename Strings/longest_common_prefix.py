ac38225e422242e972708c256cf879d99d80bee3
Akshat Jain <akshatjain030722@gmail.com>
2026-05-30 09:40:13 +0530

    longest common prefix

--- Strings/longest_common_prefix.py
+++ Strings/longest_common_prefix.py
@@ -1,3 +1,11 @@
+"""
+Problem Statement: 
+Write a function to find the longest common prefix string amongst an array of strings. 
+If there is no common prefix, return an empty string.
+"""
+
+
+
 arr = ["interview","internet","internal","interval"]
 
 arr.sort()
