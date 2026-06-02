97bd7c8fddfa2ab31189d31f8a5e0b1bbbf7daad
Akshat Jain <akshatjain030722@gmail.com>
2026-06-02 06:00:21 +0530

    check if two strings are anagrams of each other

--- Strings/check_if_anagrams.py
+++ Strings/check_if_anagrams.py
@@ -29,6 +29,12 @@ w2 = "TEGERNI"
 #     print("Yes, the strings are anagrams of each other")
 # else:
 #     print("No, the strings are not anagrams of each other")
+"""
+Here in this code, we are first creating two empty hash maps to store the frequency of each character in both strings.
+Then we are iterating through both strings and counting the frequency of each character and storing it in the respective hash maps.
+Finally, we are comparing the two hash maps. If they are equal, then we can say that the strings are anagrams of each other.
+"""
+
 
 
 # Approach 2: using a single hash map to count the frequency of each character in both strings and comparing the counts
@@ -53,3 +59,13 @@ else:
             break
     else:
         print("Yes, the strings are anagrams of each other")
+"""
+Here in this code, we are first creating an empty hash map to store the frequency of each character in both strings.
+Then we are iterating through the first string and counting the frequency of each character and storing it in the hash map.
+Then we are iterating through the second string and checking if the character is present in the hash map or not.
+If it is not present, then we can say that the strings are not anagrams of each other.
+If it is present, then we are decrementing the frequency of that character in the hash map by 1.
+If the frequency of any character becomes negative, then we can say that the strings are not anqrams of each other.
+Finally, if we have iterated through the second string and we have not found any character that is not present
+in the hash map or any character that has negative frequency, then we can say that the strings are anagrams of each other.
+"""
