48e827f4b51666719b78e861b4e78bae8603eff0
Akshat Jain <akshatjain030722@gmail.com>
2026-06-03 05:54:52 +0530

    Sort characters by Frequency

--- Strings/sort_by_frequency.py
+++ Strings/sort_by_frequency.py
@@ -18,3 +18,10 @@ sorted_char = sorted(s_dict.items(),key = lambda x: (-x[1],x[0]))
 
 result = [char for char,freq in sorted_char]
 print(result)
+"""
+Here first i have taken the string input and then i have created a empty dictionary.
+then i have added the values in the dictionary
+while sorting i have converted the dictionary into a list of tuples and then i have sorted the list of tuples by using the sorted function.
+Here in the sorted function, i have used a lambda function as a key where i have set the condition that first we will sort by frequency in descending order and then if the frequency is same then we will sort by alphabetic order.
+after sorting, i have created a list comprehension to get the characters from the sorted list of tuples and then i have printed the result.
+"""
