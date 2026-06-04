e12adf9075d6f0029f09348c8c2ccdf9f815d49f
Akshat Jain <akshatjain030722@gmail.com>
2026-06-04 05:44:28 +0530

    maximum nesting depth of parenthesis

--- Strings/maximum_nesting_depth_of_parenthesis.py
+++ Strings/maximum_nesting_depth_of_parenthesis.py
@@ -15,3 +15,10 @@ for ch in s:
     elif ch == ')':
         count -= 1
 print(ans)
+"""
+Here we have a string of parentheses and we have to find the maximum nesting depth of the parentheses
+We will keep a count variable which will keep track of the number of open parentheses.
+if the character is open parentheses then we will increment the count variable and then we will update the answer variable with the maximum of the current answer and the count variable.
+If the character is closed parentheses then we will decrement the count variable.
+Finally we will return the answer variable which will have the maximum nesting depth of the parentheses.
+"""
