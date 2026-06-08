45ae5760ad8ac09d22912fb9c679af052701dd63
Akshat Jain <akshatjain030722@gmail.com>
2026-06-08 05:59:48 +0530

    Reverse words in a string.

--- Strings/reverse_word_in_string.py
+++ Strings/reverse_word_in_string.py
@@ -1,3 +1,13 @@
+"""
+Problem statement:
+Given an input string, containing upper-case and lower-case letters, digits, and spaces( ' ' ).
+A word is defined as a sequence of non-space characters.
+The words in s are separated by at least one space.
+Return a string with the words in reverse order, concatenated by a single space.
+"""
+
+
+
 s = " Welcome to the jungle "
 
 
@@ -40,3 +50,15 @@ def reverse_words(s):
     return result
 
 print(reverse_words(s))
+""""
+Here in this code, we are first initializing an empty string result to store the final output.
+Then we are initializing a variable i to the last index of the string s.
+Then we are running a loop until i is greater than or equal to 0.
+Inside the loop, we are first ignoring any trailing spaces until we find a non-space character.
+If we reach the beginning of the string, we break out of the loop.
+Then we are setting the end variable to the current index i, which marks the end of a word.
+Then we are running another loop to find the start of the word by moving i backwards until we find a space or reach the beginning of the string.
+Then we are extracting the word using slicing and adding it to the result string.
+If the result string is not empty, we add a space before adding the new word to ensure that the words are separated by a single space.
+Finally, we return the result string which contains the words in reverse order.
+"""
