4c800c14c9e4b57bad21f6d19ab49756a72e1443
Akshat Jain <akshatjain030722@gmail.com>
2026-06-11 05:59:36 +0530

    Count Number of Substring

--- Strings/count_number_of_substring.py
+++ Strings/count_number_of_substring.py
@@ -29,3 +29,14 @@ for i in range(n):
             break
 
 print(ans)
+"""
+Here we have a string and a positive integer k and we have to find the number of substrings that contain exactly k distinct characters.
+We will keep a frequency dictionary to keep track of the frequency of each character in the current substring
+and a variable distinct to keep track of the number of distinct characters in the current substring.
+We will use two loops to generate all the substrings of the string.
+In the inner loop, we will update the frequency dictionary and the distinct variable for the current character.
+Then we will check if the number of distinct characters is equal to k or not.
+If it is equal to k, then we will increment the answer variable by 1.
+If it is greater than k, then we will break the inner loop as we are only interested in substrings that contain exactly k distinct characters.
+Finally, we will return the answer variable which will have the number of substrings that contain exactly k distinct characters.
+"""
