8dc0db28a7ac2eaa617d442b1c8f89216a20978e
Akshat Jain <akshatjain030722@gmail.com>
2026-06-11 05:54:38 +0530

    Count Number of Substring

---
+++ Strings/count_number_of_substring.py
@@ -0,0 +1,23 @@
+s = "pqpqs"
+k = 2
+ans = 0
+n = len(s)
+
+for i in range(n):
+    freq = {}
+    distinct = 0
+    for j in range(i, n):
+        ch = s[j]
+
+        if ch not in freq:
+            freq[ch] = 1
+            distinct += 1
+        else:
+            freq[ch] += 1
+        
+        if distinct == k:
+            ans += 1
+        elif distinct > k:
+            break
+
+print(ans)
