270108c9b31cb6cd378e73e12cfd308fabb2b9cf
Akshat Jain <akshatjain030722@gmail.com>
2026-07-03 06:16:56 +0530

    Remove N-th node from the end of a Linked List

---
+++ Linked List/Singly Linked List/remove_nth_node.py
@@ -0,0 +1,41 @@
+class node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+class solution:
+    def printLL(self,head):
+        while head is not None:
+            print(head.data, end = " ")
+            head = head.next
+
+    def remove_node(self, head, N):
+        dummy = node(0,head)
+        slow = dummy
+        fast = dummy
+
+        for _ in range(N+1):
+            fast = fast.next
+        
+        while fast is not None:
+            slow = slow.next
+            fast = fast.next
+        
+        slow.next = slow.next.next
+
+        return dummy.next
+
+if __name__ == '__main__':
+    arr = [1,2,3,4,5]
+    N = 3
+
+    head = node(arr[0])
+    head.next = node(arr[1])
+    head.next.next = node(arr[2])
+    head.next.next.next = node(arr[3])
+    head.next.next.next.next = node(arr[4])
+
+    sol = solution()
+    head = sol.remove_node(head, N)
+    sol.printLL(head)
+    
