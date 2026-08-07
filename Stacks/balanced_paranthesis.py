63fdb84692dbed91434406b87892e5b133c3deb1
Akshat Jain <akshatjain030722@gmail.com>
2026-08-07 06:17:45 +0530

    Check for balanced parentheses

--- Stacks/balanced_paranthesis.py
+++ Stacks/balanced_paranthesis.py
@@ -1,3 +1,12 @@
+"""
+Problem Statement:
+Check Balanced Parentheses.
+Given string str containing just the characters '(', ')', '{', '}', '[' and ']',
+check if the input string is valid and return true if the string is balanced otherwise return false.
+"""
+
+
+
 class ArrayStack:
     def __init__(self, size=1000):
         self.stackArray = [0] * size
