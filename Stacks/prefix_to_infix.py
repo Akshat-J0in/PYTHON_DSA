491a6447d4b4c3152076a85a8dfee77a0faaa65b
Akshat Jain <akshatjain030722@gmail.com>
2026-08-17 06:15:10 +0530

    Prefix to infix

---
+++ Stacks/prefix_to_infix.py
@@ -0,0 +1,20 @@
+# Now this is the main fuction where we are going to convert the prefix to infix
+def prefix_to_infix(prefix):
+    stack = []
+    for c in reversed(prefix):
+        if c.isalnum():
+            stack.append(c)
+        else:
+            op1 = stack.pop()
+            op2 = stack.pop()
+
+            stack.append(f"({op1}{c}{op2})")
+    return stack[-1]
+
+# The main function where we are setting our prefix expression and passing it to our main function
+def main():
+    prefix = "*-A/BC-/AKL"
+    print("Infix expression: ", prefix_to_infix(prefix))
+
+if __name__ == "__main__":
+    main()
