ec1d4b2c923bacdd164b95e12ad7a3e91508bfa6
Akshat Jain <akshatjain030722@gmail.com>
2026-08-19 06:08:01 +0530

    Postfix to prefix conversion

---
+++ Stacks/postfix_to_prefix.py
@@ -0,0 +1,21 @@
+# Now here we are going to create a function that will convert postfix to prefix
+def postfix_to_prefix(postfix):
+    stack = []
+    for c in postfix:
+        if c.isalnum():
+            stack.append(c)
+
+        else:
+            op2 = stack.pop()
+            op1 = stack.pop()
+
+            stack.append(c + op1 + op2)
+
+    return stack[-1]
+
+def main():
+    postfix = "ABC/-AK/L-*"
+    print("Prefix Expression: ", postfix_to_prefix(postfix))
+
+if __name__ == "__main__":
+    main()
